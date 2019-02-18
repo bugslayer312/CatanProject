b4f3e1a91c25c71ffda080aad861a6c1b8815123
Eugene Moskalev <bugslayer312@gmail.com>
2019-02-18 22:53:06 +0500

    Some arrangments

--- build.sh
+++ build.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
+build_type=$1
+if [ -z $build_type ]; then
+  build_type=Debug
+fi
+
+
 for dir in build output; do
+  if [ ! -d $dir ]; then
+    mkdir $dir
+  fi
+done
+
+for dir in build/$build_type output/$build_type; do
   if [ ! -d $dir ]; then
     mkdir $dir
   fi
   rm -r $dir/*
 done
 
-cd build
-cmake -DCMAKE_PREFIX_PATH="$QT_PREFIX_PATH" ../src
+cd build/$build_type
+cmake -DCMAKE_PREFIX_PATH="$QT_PREFIX_PATH" -DCMAKE_BUILD_TYPE="$build_type" ../../src
 make
